c7fe193ca8243cc22f74fce2d1d365f09c8ebe52
Pruthvi <psambu@martinonesource.com>
2021-01-12 10:10:20 -0600

    Minor changes to google authentication and adding redux dev tools extension

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 
 import App from './components/App';
 import Reducers from './reducers';
 
-const store = createStore(Reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(Reducers, composeEnhancers(applyMiddleware()));
+
 
 ReactDOM.render(
     <Provider store = {store}>
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,14 +1,16 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
 const INITIAL_STATE = {
-    isSignedIn : null
+    isSignedIn : null,
+    userId: null
 }
 
 export const authReducer = (state = INITIAL_STATE, actions) => {
     switch(actions.type) {
-        case 'SIGN_IN': {
-            return { ...state, isSignedIn: true};
+        case SIGN_IN: {
+            return { ...state, isSignedIn: true, userId: actions.payload};
         }
-        case 'SIGN_OUT': {
-            return { ...state, isSignedIn: false};
+        case SIGN_OUT: {
+            return { ...state, isSignedIn: false, userId: null};
         }
         default: {
             return state;
