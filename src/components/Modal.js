f076799a43c43cf92d494f4f18e350954163f4c2
Pruthvi <psambu@martinonesource.com>
2021-01-28 07:13:47 -0600

    minor changes to modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const Modal = (props) => {
     return ReactDOM.createPortal(
-        <div className = 'ui dimmer modals visible active' onClick = {() => history.push('/')}>
+        <div className = 'ui dimmer modals visible active' onClick = {props.onDismiss}>
+            {/* e.stopPropagation to Prevent click function bubbling up */}
             <div className = 'ui standard modal visible active' onClick = {(e) => e.stopPropagation()}>
                 <div className = 'header'>{props.title}</div>
                 <div className = 'content'>{props.content}</div>
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Modal from '../Modal';
-
+import history from '../../history';
 const StreamDelete = (props) => {
     const actions = (
         <React.Fragment>
@@ -10,7 +10,7 @@ const StreamDelete = (props) => {
     );
     return (
         <div>
-            <Modal title = 'Delete Stream' content = 'Are you sure you want to delete this stream' actions = {actions}/>
+            <Modal title = 'Delete Stream' content = 'Are you sure you want to delete this stream' actions = {actions} onDismiss = {() =>  history.push('/')}/>
         </div>
     );
 }
